40743aa8759680aa052fb9e7e88b53c64b0b65d4
closingsquarebracket <49586771+closingsquarebracket@users.noreply.github.com>
2020-08-12 00:29:32 +0200

    Extended Metadata extraction from articles
    Also logging capabilities. First run over 12k articles successful, looks stable. Small clean-up to original file, grouping of similar functions.

--- medical_importer.py
+++ medical_importer.py
@@ -1,5 +1,4 @@
 import requests
-import os
 from lxml import etree
 import datetime
 import re
@@ -11,19 +10,18 @@ with open('locations.json', 'r') as f_loc:
     locations = json.load(f_loc)
 base_directory = locations['base_directory']
 base_request_url = locations['base_request_url']
+
+parse_locations = {"journal_name": "front/journal-meta/journal-id[@journal-id-type='nlm-ta']",
+    "article_category": "front/article-meta/article-categories/subj-group/subject",
+    "article_title": "front/article-meta/title-group/article-title",
+    "abstract": "front/article-meta/abstract/p"}
+
+
 def get_pmc_id(file):
     """extract the ID from the file name"""
     pmc_id = file.split(".")[0]
     return pmc_id[3:]
 
-def get_journal_name(parsed_document):
-    """extract the journal name based on a parsed document. If no name can be extracted, return 'n/a'."""
-    try:
-        journal_name = parsed_document.find("front/journal-meta/journal-id[@journal-id-type='nlm-ta']").text
-    except AttributeError:
-        # capture occasional parsing error
-        journal_name = "n/a"
-    return journal_name
 
 def get_publication_date(parsed_document):
     """extract the publication date. returns 'n/a' for non-parseable dates."""
@@ -48,14 +46,14 @@ def get_publication_date(parsed_document):
             except AttributeError:
                 # parsing error or other publication method
                 pub_date = None
-                pub_year, pub_month, pub_day = 1, 1, 1 # silce complaints from IDE
+                pub_year, pub_month, pub_day = 1, 1, 1  # silce complaints from IDE
     if pub_date is not None:
         # parse date into standard format
         pub_date = datetime.date(int(pub_year), int(pub_month), int(pub_day))
     return pub_date
 
+
 def get_citation_count(pmc_id):
-    # TODO: Bottleneck in retrieval. The API allows retrieval of info in blocks of up to 500. Need to rework this to allow for block access.
     anwer = requests.get(f"{base_request_url}dbfrom=pubmed&linkname=pubmed_pmc_refs&id={pmc_id}&retmode=json")
     try:
         number_of_citations = len(anwer.json()['linksets'][0]['linksetdbs'][0]['links'])
@@ -63,6 +61,7 @@ def get_citation_count(pmc_id):
         # if there are no citations, the 'linksetdbs' key is not set, leading to a key error instead of an empty list.
         number_of_citations = 0
     return number_of_citations
+
 
 def get_multiple_citations(list_of_ids):
     """retrieve citation lists for multiple input ids"""
@@ -77,8 +76,57 @@ def get_multiple_citations(list_of_ids):
             citations = list_of_links[counter]['linksetdbs'][0]['links']
         except KeyError:
             citations = []
-        list_of_citations.append(citations) # list of lists: [[<citations>], [<citations>]] in the same order as input list
+        list_of_citations.append(
+            citations)  # list of lists: [[<citations>], [<citations>]] in the same order as input list
     return list_of_citations
+
+
+def get_article_info(parsed_document: etree, xml_location: str, alt_text: str = 'n/a'):
+    try:
+        information = parsed_document.find(xml_location).text
+    except AttributeError:
+        information = alt_text
+    if information is None:
+        information = alt_text
+    return information
+
+
+def get_authors(parsed_document, min_number_authors):
+    list_of_authors = []
+    try:
+        authors = parsed_document.findall('front/article-meta/contrib-group/')
+        try:
+            for author in authors:
+                surname = author.find('name/surname').text
+                given_name = author.find('name/given-names').text
+                list_of_authors += [surname, given_name]
+        except AttributeError:
+            # Error in Parsing
+            list_of_authors += ['ParsingError'] * 2
+    except AttributeError:
+        # list of authors cannot be found
+        list_of_authors += ['MissingAuthor'] * 2
+
+    if len(list_of_authors) <= 2 * min_number_authors:
+        # padding if there is only one or two authors:
+        list_of_authors += [' - '] * 2 * min_number_authors
+    return list_of_authors[:2 * min_number_authors]
+
+
+def get_keywords(parsed_document):
+    list_of_keywords = []
+    try:
+        kwds = parsed_document.findall('front/article-meta/kwd-group/kwd')
+        for kwd in kwds:
+            try:
+                keyword = kwd.text
+                list_of_keywords.append(keyword)
+            except AttributeError:
+                list_of_keywords.append('n/a')
+    except AttributeError:
+        list_of_keywords = ['n/a']
+    return list_of_keywords
+
 
 def clean_index_file():
     with open('oa_file_list.txt', 'r') as f_original:
@@ -87,17 +135,16 @@ def clean_index_file():
     publication_dates = []
     pmcs = []
     unparseable = 0
-    for line in file_lines[1:]: # leave out initial empty line
+    for line in file_lines[1:]:  # leave out initial empty line
         location, publication, pmc, *_ = line.split('\t')
         try:
-            _, publication_name, publication_date, *_ = re.split(r"([\w\s()]+.?)\s(\d{4}\s\w{3}\s\d+);", publication) # :( re is expensive
-            # 25% unparseable
+            _, publication_name, publication_date, *_ = re.split(r"([\w\s()]+.?)\s(\d{4}\s\w{3}\s\d+);",
+                                                                 publication)  # :( re is expensive  # 25% unparseable
         except ValueError:
             try:
                 _, publication_name, publication_date, *_ = re.split(r"([\w\s()]+.?)\s(\d{4}\s\w{3})[-;\s]",
                                                                      publication)  # :( double re is expensive
-                publication_date += ' 1' # account for missing dates
-                #down to 2% unparseable
+                publication_date += ' 1'  # account for missing dates  # down to 2% unparseable
             except ValueError:
                 print(line)
                 unparseable += 1
@@ -107,12 +154,13 @@ def clean_index_file():
         publication_dates.append(publication_date)
         pmcs.append(pmc)
     print(len(pmcs))
-    print(f"unparseable = {100*unparseable/len(pmcs)}%.")
+    print(f"unparseable = {100 * unparseable / len(pmcs)}%.")
 
     with open('oa_wo_citations.txt', 'w+') as f_wo:
         f_wo.write('PMC_ID; Publication; Publication_date\n')
         for counter in range(len(pmcs)):
             f_wo.write(f"{pmcs[counter]}; {publication_names[counter]}; {publication_dates[counter]}\n")
+
 
 if __name__ == '__main__':
     # iterate over all files under the base directory.
@@ -131,7 +179,7 @@ if __name__ == '__main__':
     #                 tree = etree.parse(file_address)
     #                 root = tree.getroot()
     #                 # retrieve standard journal name
-    #                 journal_name = get_journal_name(root)
+    #                 journal_name = get_article_info(root, parse_locations['journal_name'])
     #                 # find publication date
     #                 pub_date = get_publication_date(root)
     #                 # get citation count
@@ -180,7 +228,7 @@ if __name__ == '__main__':
     publication_dates = []
 
     for line in lines[1:]:
-        pmc, publication_name, publication_date = line.strip().split("; ") #remove \n after date
+        pmc, publication_name, publication_date = line.strip().split("; ")  # remove \n after date
         pmcs.append(pmc)
         publication_names.append(publication_name)
         publication_dates.append(publication_date)
@@ -189,33 +237,25 @@ if __name__ == '__main__':
     full_web_of_citations = {}
     with open('full_web.json', 'r') as f_web:
         old_web = json.load(f_web)  # read old information from file
-    full_web_of_citations = {**old_web, **full_web_of_citations} # join dictionaries
+    full_web_of_citations = {**old_web, **full_web_of_citations}  # join dictionaries
     del old_web
     print(f"number of papers: {len(pmcs)}, currently processed: {len(full_web_of_citations)}")
 
-    for block in range(2399000, len(pmcs), citation_block_size): # in case of crash, restart from last printed 'start' value
+    for block in range(2399000, len(pmcs),
+                       citation_block_size):  # in case of crash, restart from last printed 'start' value
         start = block
         print(start)
-        end = min(len(pmcs), block + citation_block_size) # avoid overshooting and having to rerun.
+        end = min(len(pmcs), block + citation_block_size)  # avoid overshooting and having to rerun.
         source_papers = pmcs[start:end]
         citations = get_multiple_citations(pmcs[start:end])
-        for counter in range(end-start):
+        for counter in range(end - start):
             n_citations.append(len(citations[counter]))
             full_web_of_citations[source_papers[counter]] = citations[counter]
 
         with open('oa_with_citations.txt', 'a') as f_table:
-            for counter in range(start,end):
-                f_table.write(f"{pmcs[counter]}; {publication_names[counter]}; {publication_dates[counter]}; {n_citations[counter - start]}\n")
+            for counter in range(start, end):
+                f_table.write(
+                    f"{pmcs[counter]}; {publication_names[counter]}; {publication_dates[counter]}; {n_citations[counter - start]}\n")
 
         with open('full_web.json', 'w') as f_web:
             json.dump(full_web_of_citations, f_web)
-
-
-
-
-
-
-
-    # TODO: It may be possible to retrieve metadata and citation count in one. Need to study Entrez syntax.
-
-
---
+++ meta_data_preparation.py
@@ -0,0 +1,55 @@
+import medical_importer as mi
+import os
+from lxml import etree
+import logging
+
+n_authors = 3
+
+if __name__ == '__main__':
+    logging.basicConfig(filename = 'LogFile.txt', datefmt = 'd %b %Y %H:%M:%S', level = logging.INFO)
+
+    citation_connection = {}
+    # for that sweet O(1) speed
+
+    with open('oa_with_citations.txt', 'r') as f_citations:
+        for line in f_citations:
+            if line[0] in '0123456789':
+                PMC_ID, _, pub_date, n_citations = line.strip().split('; ')
+                citation_connection[PMC_ID] = {'citations': int(n_citations), 'pub_date': pub_date}
+            else:
+                logging.warning(f'Unparseable line from citation file: {line}')
+                pass
+    logging.info('citation matching successfully parsed.')
+    for subdir, dir, files in os.walk(os.path.join(mi.base_directory, 'comm_use.A-B.xml')):
+        file_empty = False
+        with open('metadata_matching.txt', 'r') as f_meta:
+            for line_no, line in enumerate(f_meta):
+                if len(line) == 0 and line_no < 4:
+                    file_empty = True
+        if file_empty:
+            logging.warning("File found empty. Emptying file and recreating header.")
+            with open('metadata_matching.txt', 'w') as f_meta:
+                f_meta.write(
+                    "PMC_ID; publication_date; journal; article; article_category; author1_sur; author1;_given; author2_sur; author2_given; author3_sur, author3_given; keywords; citations\n")
+        for file in files:
+            if file != '.DS_Store':
+                logging.info(f'Parsing file {file}')
+                file_address = os.path.join(subdir, file)
+                root = etree.parse(file_address).getroot()
+                # retrieve metadata
+                pmc_id = mi.get_pmc_id(file)
+                try:
+                    citations = citation_connection[pmc_id]['citations']
+                    pub_date = citation_connection[pmc_id]['pub_date']
+                except KeyError:
+                    # skip articles that don't have citation data available
+                    continue
+                journal_name = mi.get_article_info(root, mi.parse_locations['journal_name'])
+                article_title = mi.get_article_info(root, mi.parse_locations['article_title']).strip('\n\t;')
+                article_category = mi.get_article_info(root, mi.parse_locations['article_category'])
+                authors = mi.get_authors(root, n_authors)
+                keywords = mi.get_keywords(root)
+                # abstract = mi.get_article_info(root, mi.parse_locations['abstract'], alt_text = 'No abstract could be parsed')
+                with open('metadata_matching.txt', 'a') as f_meta:
+                    f_meta.write(
+                        f'{pmc_id}; {pub_date}; {journal_name}; {article_title}; {authors[0]}; {authors[1]}; {authors[2]}; {authors[3]}; {authors[4]}; {authors[5]}; {keywords}; {citations}\n')
